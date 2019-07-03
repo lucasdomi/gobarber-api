cd39a9521eef60707ef8673b2a3d792024d0ac80
Lucas Domingues <lucasdomi@globo.com>
2019-07-03 09:37:33 -0300

    Listando notificacoes do usuario

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +23,7 @@ routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
