d82d36afe607aeb259f0b37faea9f4fe8f9099da
Lucas Domingues <lucasdomi@globo.com>
2019-07-03 08:57:03 -0300

    Configurando o mongodb

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,7 +21,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
-
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
