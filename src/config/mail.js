14359d17f6502c64134663f66c3413838d69478b
Lucas Domingues <lucasdomi@globo.com>
2019-07-03 23:37:45 -0300

    Configurando templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '5893fdaca0b39a',
+    pass: 'cfc32876dce01f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
