f2277425b465d87fa26df0e5c52d4353ad5d1925
Lucas Domingues <lucasdomi@globo.com>
2019-10-08 01:04:36 -0300

    Refatora controle de usuario e incrementa gitignore

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,13 +15,17 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation failed' });
+      return res.status(400).json({ error: 'Validation fails' });
     }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
+
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
+
     const { id, name, email, provider } = await User.create(req.body);
+
     return res.json({
       id,
       name,
@@ -46,20 +50,21 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation failed' });
+      return res.status(400).json({ error: 'Validation fails' });
     }
 
     const { email, oldPassword } = req.body;
+
     const user = await User.findByPk(req.userId);
 
     if (email !== user.email) {
-      const userExists = await User.findOne({
-        where: { email },
-      });
+      const userExists = await User.findOne({ where: { email } });
+
       if (userExists) {
         return res.status(400).json({ error: 'User already exists.' });
       }
     }
+
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
@@ -71,7 +76,7 @@ class UserController {
         {
           model: File,
           as: 'avatar',
-          attributes: ['id', 'path', 'avatar'],
+          attributes: ['id', 'path', 'url'],
         },
       ],
     });
